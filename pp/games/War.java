4809a17c2fd70166637baa548ca2000328335a45
farisNassif <g00347032@gmit.ie>
2019-02-26 17:16:51 +0000

    Repackaging repository

--- pp/games/War.java
+++ pp/games/War.java
@@ -11,12 +11,14 @@ import java.io.PrintWriter;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
+import pp.services.*;
 
 // Cormac Raftery & Faris Nassif
 // Class works, need to make it nice and clean though maybe make it smaller by creating some 
 // other classes + methods for delegation
 public class War {
 
+	private final static int dealtCardsAmount = 13;
 	static int pointsOnTheBattlefield;
 	static int[] shuffledDeck = new int[156];
 	static int num;
@@ -35,158 +37,162 @@ public class War {
 	private static Scanner console;
 
 	public static void main(String[] args) {
-		{
 
-			console = new Scanner(System.in);
+		console = new Scanner(System.in);
 
-			System.out.printf("Would you like to either: \n1.Start a new game\n2.Load the previously saved game\n");
-			roundOption = console.nextInt();
-			if (roundOption == 1) {
-				System.out.printf("Please enter the number of players(2-10):");
-				players = console.nextInt();
-				if (players < 5)// shuffles from one deck
-				{
-					shuffledeck();
-					deal();
-				} else if (players < 9)// shuffles from two decks
-				{
-					shuffledeck();
-					for (i = 0; i < 52; i++) {
-						shuffledDeck[i + 52] = shuffledDeck[i];
-					}
-					shuffledeck();
-					deal();
-				} else// shuffles from three decks
-				{
-					shuffledeck();
-					for (i = 0; i < 52; i++) {
-						shuffledDeck[i + 104] = shuffledDeck[i];
-					}
-					shuffledeck();
-					for (i = 0; i < 52; i++) {
-						shuffledDeck[i + 52] = shuffledDeck[i];
-					}
-					shuffledeck();
-					deal();
+		System.out.printf("Would you like to either: \n1.Start a new game\n2.Load the previously saved game\n");
+		roundOption = console.nextInt();
+		if (roundOption == 1) {
+			System.out.printf("Please enter the number of players(2-10):");
+			players = console.nextInt();
+			if (players < 5)// shuffles from one deck
+			{
+				Services.shuffledeck(j, num, shuffledDeck);
+				deal();
+			} else if (players < 9)// shuffles from two decks
+			{
+				Services.shuffledeck(j, num, shuffledDeck);
+				for (i = 0; i < 52; i++) {
+					Services.shuffledeck(j, num, shuffledDeck);
 				}
-			} else {
-				// filep = fopen("gamestate.txt", "r");//opens the file to read
-				BufferedReader filep = null;
-				try {
-					filep = new BufferedReader(new InputStreamReader(new FileInputStream("gamestate.txt")));
-				} catch (FileNotFoundException e) {
-					System.out.println(e);
+				Services.shuffledeck(j, num, shuffledDeck);
+				deal();
+			} else// shuffles from three decks
+			{
+				Services.shuffledeck(j, num, shuffledDeck);
+				for (i = 0; i < 52; i++) {
+					shuffledDeck[i + 104] = shuffledDeck[i];
 				}
-				String line = "";
-				try {
-					if ((line = filep.readLine()) == null) {
-						System.out.printf("The file cannot be opened\n");
-					} else {
+				Services.shuffledeck(j, num, shuffledDeck);
+				for (i = 0; i < 52; i++) {
+					shuffledDeck[i + 52] = shuffledDeck[i];
+				}
+				Services.shuffledeck(j, num, shuffledDeck);
+				deal();
+			}
+		} else {
+			BufferedReader filep = null;
+			try {
+				filep = new BufferedReader(new InputStreamReader(new FileInputStream("gamestate.txt")));
+			} catch (FileNotFoundException e) {
+				System.out.println(e);
+			}
+			String line = "";
+			try {
+				if ((line = filep.readLine()) == null) {
+					System.out.printf("The file cannot be opened\n");
+				} else {
 
-						players = Integer.parseInt(line);
+					players = Integer.parseInt(line);
+					line = filep.readLine();
+					pointsOnTheBattlefield = Integer.parseInt(line);
+					line = filep.readLine();
+					roundNum = Integer.parseInt(line);
+					line = filep.readLine();
+					for (i = 0; i < players; i++) {
+						playerPoints[i] = Integer.parseInt(line);
 						line = filep.readLine();
-						pointsOnTheBattlefield = Integer.parseInt(line);
-						line = filep.readLine();
-						roundNum = Integer.parseInt(line);
-						line = filep.readLine();
-						for (i = 0; i < players; i++) {
-							playerPoints[i] = Integer.parseInt(line);
+					}
+					for (i = 0; i < players; i++) {
+						for (j = 0; j < 13; j++) {
+							hands[i][j] = Integer.parseInt(line);
 							line = filep.readLine();
+						}
+					}
+				}
+			} catch (NumberFormatException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			// Closes the file
+			try {
+				filep.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		while (roundNum > 0)// detects if the game is finished
+		{
+			System.out.printf(
+					"Would you like to: \n1.Complete the next round\n2.Save the game\n3.Output the game status\n4.Exit the game\n");
+			roundOption = console.nextInt();
+			while (roundOption != 1) {
+				switch (roundOption) {
+				case 1:// next round
+					break;
+				case 2:
+					// Saves the game
+					try (FileWriter fw = new FileWriter("gamestate.txt", false);
+							BufferedWriter bw = new BufferedWriter(fw);
+							PrintWriter out = new PrintWriter(bw)) {
+						out.println(players);
+						out.println(pointsOnTheBattlefield);
+						out.println(roundNum);
+						for (i = 0; i < players; i++) {
+							out.println(playerPoints[i]);
 						}
 						for (i = 0; i < players; i++) {
 							for (j = 0; j < 13; j++) {
-								hands[i][j] = Integer.parseInt(line);
-								line = filep.readLine();
+								out.println(hands[i][j]);
 							}
 						}
+						out.close();
+					} catch (IOException e) {
+						e.printStackTrace();
 					}
-				} catch (NumberFormatException e1) {
-					e1.printStackTrace();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-				// close(filep);//close the file
-				try {
-					filep.close();
-				} catch (IOException e) {
-					e.printStackTrace();
+					System.out.printf(
+							"Would you like to: \n1.Complete the next round\n2.Save the game\n3.Output the game status\n4.Exit the game\n");
+					roundOption = console.nextInt();
+					break;
+				case 3:
+					// Show Score
+					for (i = 1; i <= players; i++) {
+						System.out.printf("Player %d has %d points\n", i, playerPoints[i - 1]);
+					}
+					System.out.printf(
+							"Would you like to: \n1.Complete the next round\n2.Save the game\n3.Output the game status\n4.Exit the game\n");
+					roundOption = console.nextInt();
+					break;
+				case 4:
+					// Exits the Game
+					System.exit(0);
+					break;
+				default:
+					System.out.printf(
+							"Would you like to: \n1.Complete the next round\n2.Save the game\n3.Output the game status\n4.Exit the game\n");
+					roundOption = console.nextInt();
+					break;
 				}
 			}
-
-			while (roundNum > 0)// detects if the game is finished
+			for (i = 1; i <= players; i++)// each player selects card
 			{
-				System.out.printf(
-						"Would you like to: \n1.Complete the next round\n2.Save the game\n3.Output the game status\n4.Exit the game\n");
-				roundOption = console.nextInt();
-				while (roundOption != 1) {
-					switch (roundOption) {
-					case 1:// next round
-						break;
-					case 2:// save
-							// playerPoints playerhands roundNum players and points on the battlefield
-						try (FileWriter fw = new FileWriter("gamestate.txt", false);
-								BufferedWriter bw = new BufferedWriter(fw);
-								PrintWriter out = new PrintWriter(bw)) {
-							out.println(players);
-							out.println(pointsOnTheBattlefield);
-							out.println(roundNum);
-							for (i = 0; i < players; i++) {
-								out.println(playerPoints[i]);
-							}
-							for (i = 0; i < players; i++) {
-								for (j = 0; j < 13; j++) {
-									out.println(hands[i][j]);
-								}
-							}
-							out.close();
-						} catch (IOException e) {
-							// exception handling left as an exercise for the reader
-						}
-						System.out.printf(
-								"Would you like to: \n1.Complete the next round\n2.Save the game\n3.Output the game status\n4.Exit the game\n");
-						roundOption = console.nextInt();
-						break;
-					case 3:// show score
-						for (i = 1; i <= players; i++) {
-							System.out.printf("Player %d has %d points\n", i, playerPoints[i - 1]);
-						}
-						System.out.printf(
-								"Would you like to: \n1.Complete the next round\n2.Save the game\n3.Output the game status\n4.Exit the game\n");
-						roundOption = console.nextInt();
-						break;
-					case 4:// exit app
-						System.exit(0);
-						break;
-					default:
-						System.out.printf(
-								"Would you like to: \n1.Complete the next round\n2.Save the game\n3.Output the game status\n4.Exit the game\n");
-						roundOption = console.nextInt();
-						break;
-					}
-				}
-				for (i = 1; i <= players; i++)// each player selects card
-				{
-					playround(i);
-				}
-				roundNum--;
-				whowins();// determines the highest unique card
-				totalRoundPoints = 0;
-				for (i = 1; i <= players; i++) {
-					if (playersCard[i - 1] <= 10) {
-						System.out.printf("Player %d played: %d\n", i, playersCard[i - 1]);
-					} else if (playersCard[i - 1] == 11) {
-						System.out.printf("Player %d played: J\n", i);
-					} else if (playersCard[i - 1] == 12) {
-						System.out.printf("Player %d played: Q\n", i);
-					} else if (playersCard[i - 1] == 13) {
-						System.out.printf("Player %d played: K\n", i);
-					} else if (playersCard[i - 1] == 14) {
-						System.out.printf("Player %d played: A\n", i);
-					}
+				playRound(i);
+			}
+			roundNum--;
+			// Determines the highest unique card value
+			whowins();
+			totalRoundPoints = 0;
+			// Print out players played card
+			for (i = 1; i <= players; i++) {
+				// Handles cards 2-10
+				if (playersCard[i - 1] <= 10) {
+					System.out.printf("Player %d played: %d\n", i, playersCard[i - 1]);
+					// Handles cards J - A
+				} else if (playersCard[i - 1] == 11) {
+					System.out.printf("Player %d played: J\n", i);
+				} else if (playersCard[i - 1] == 12) {
+					System.out.printf("Player %d played: Q\n", i);
+				} else if (playersCard[i - 1] == 13) {
+					System.out.printf("Player %d played: K\n", i);
+				} else if (playersCard[i - 1] == 14) {
+					System.out.printf("Player %d played: A\n", i);
 				}
 			}
 		}
-	}
+
+	} // Main Method
 
 	public static void whowins() {
 		for (i = 0; i < players; i++) {
@@ -226,7 +232,7 @@ public class War {
 
 	}
 
-	public static void playround(int playerTurn) {
+	public static void playRound(int playerTurn) {
 		System.out.printf("Player %d enter any number when ready:\n", playerTurn);// used to conceal cards from previous
 																					// player
 		j = console.nextInt();
@@ -251,36 +257,17 @@ public class War {
 		for (j = playerChoice; j < 12; j++) {
 			hands[(playerTurn - 1)][j] = hands[(playerTurn - 1)][j + 1];// moves selected card to end of the array
 		}
-		// system("CLS");//clears terminal to hide cards
 
-	}
+	} //
 
 	public static void deal() {
 		k = 0;
-		for (i = 0; i < 13; i++) {
+		for (i = 0; i < dealtCardsAmount; i++) {
 			for (j = 0; j < players; j++) {
 				hands[j][i] = shuffledDeck[k];// deals cards realistically 1 to each player at a time,also ensures decks
 												// get mixed when playing with 2-3 decks
 				k++;
 			}
 		}
-
-	}
-
-	static void shuffledeck() {
-		int ar[] = { 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10,
-				10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 14 };
-
-		Random rnd = ThreadLocalRandom.current();
-		for (int i = ar.length - 1; i > 0; i--) {
-			int index = rnd.nextInt(i + 1);
-			// Simple swap
-			int a = ar[index];
-			ar[index] = ar[i];
-			ar[i] = a;
-		}
-		for (j = num; j < 51; ++j) {
-			shuffledDeck[j] = ar[j + 1];// copies random index number to end of array
-		}
-	}
-}
+	} // Deal Method
+} // War Class
